cc2eadfc1dbf0898e61b2d180c73365494f236c8
lzapata7 <l.zapata@alumno.um.edu.ar>
2023-04-04 12:21:30 -0300

    Separacion de test

--- romanos/romanos.py
+++ romanos/romanos.py
@@ -1,5 +1,3 @@
-import unittest
-
 def decimal_to_roman(decimal):
     if decimal <= 3:
         return 'I' * decimal
@@ -8,31 +6,7 @@ def decimal_to_roman(decimal):
     else:
         return "X"
 
-
-class TestDecimalToRoman(unittest.TestCase):
-    def test_uno(self):
-        # pre condicion
-        ### NO HAY ###
-        # proceso
-        resultado = decimal_to_roman(1)
-        # verificacion
-        self.assertEqual(resultado, 'I')
-
-    def test_diez(self):
-        resultado = decimal_to_roman(10)
-        self.assertEqual(resultado, 'X')
-
-    def test_cinco(self):
-        resultado = decimal_to_roman(5)
-        self.assertEqual(resultado, 'V')
-
-    def test_dos(self):
-        resultado = decimal_to_roman(2)
-        self.assertEqual(resultado, 'II')
-
-    def test_tres(self):
-        resultado = decimal_to_roman(3)
-        self.assertEqual(resultado, 'III')
-
 if __name__ == '__main__':
-    unittest.main()
+    print(decimal_to_roman)
+
+
